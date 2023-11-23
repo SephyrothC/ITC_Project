9d070187e7b0b540f9c66fb924d69fcf847d49b5
SephyrothC <as.grifon@gmail.com>
2023-11-23 09:28:55 +0900

    fail

--- code/main.py
+++ code/main.py
@@ -1,7 +1,9 @@
+import requests
 import csv
 from collections import defaultdict, Counter
 from fuzzywuzzy import process, fuzz
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
@@ -24,39 +26,8 @@ class Movie:
         return f"Movie ID: {self.id}\nName: {self.name}\nRating: {self.rating}\nTags: {', '.join(self.tags)}\nLink: {self.link}"
 
 
-def calculate_average_ratings():
-    # Créer un dictionnaire pour stocker les notes de chaque film
-    ratings = defaultdict(list)
-
-    # Ouvrir le fichier CSV et lire les données
-    with open('ml-25m/ml-25m/ratings.csv', 'r') as f:
-        reader = csv.reader(f)
-        next(reader)  # Skip the header row
-        for row in reader:
-            movie_id = int(row[1])
-            rating = float(row[2])
-            ratings[movie_id].append(rating)
-
-    # Calculer la note moyenne pour chaque film
-    average_ratings = {movie_id: (sum(ratings_list) / len(ratings_list) if len(ratings_list) >= 1000 else 0.5)
-                       for movie_id, ratings_list in ratings.items()}
-
-    # Trier les notes moyennes par movieId
-    sorted_ratings = sorted(average_ratings.items())
-
-    # Écrire les notes moyennes dans un nouveau fichier CSV
-    with open('ml-25m/ml-25m/average.csv', 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(['movieId', 'averageRating'])  # Write the header row
-        for movie_id, average_rating in sorted_ratings:
-            writer.writerow([movie_id, average_rating])
-
-
 def MovieInit():
-
     movies = {}
-
-    # Ouvrir le fichier CSV et lire les données
     with open('ml-25m/ml-25m/movies.csv', 'r', encoding='utf-8') as f:
         reader = csv.reader(f)
         next(reader)  # Skip the header row
@@ -65,12 +36,10 @@ def MovieInit():
             name = row[1]
             tags = row[2].split('|')  # Split the genres string into a list
             movies[id] = Movie(id, name, tags=tags)
-
     return movies
 
 
 def load_average_ratings():
-    # Ouvrir le fichier CSV et lire les données
     with open('ml-25m/ml-25m/average.csv', 'r') as f:
         reader = csv.reader(f)
         next(reader)  # Skip the header row
@@ -80,91 +49,21 @@ def load_average_ratings():
             average_ratings[movie_id] = average_rating
 
 
-def get_movies_by_name(movie_names, movies_dict, limit=3):
-    # Initialiser une liste vide pour stocker les films trouvés
-    found_movies = []
-
-    # Parcourir la liste des noms de films
-    for name in movie_names:
-        # Parcourir le dictionnaire des films
-        for movie in movies_dict.values():
-            # Si le nom du film est suffisamment proche du nom recherché, ajouter l'objet Movie à la liste
-            # Vous pouvez ajuster ce seuil en fonction de vos besoins
-            if fuzz.ratio(movie.name, name) > 60:
-                found_movies.append(movie)
-
-    # Renvoyer la liste des films trouvés
-    return found_movies
-
-
-def search_movie(name, movies_dict, limit=3):
-    # Créer une liste de noms de films à partir du dictionnaire de films
-    movie_list = [movie.name for movie in movies_dict.values()]
-
-    # Utiliser fuzzywuzzy pour trouver les films dont le nom est le plus proche de la chaîne donnée
-    results = process.extract(name, movie_list, limit=limit)
-
-    # Renvoyer les résultats
-    return results
-
-
-def search_movie_id_by_name(name, movies_dict):
-    # Utiliser une compréhension de liste pour trouver l'ID du film correspondant
-    movie_id = next((id for id, movie in movies_dict.items()
-                    if movie.name.lower() == name.lower()), None)
-
-    # Renvoyer l'ID du film trouvé ou None si aucun film n'a été trouvé
-    return movie_id
-
-
 def recommend_movies(movie_names, movies_dict, limit=5):
-
-    # Trouver les films les plus similaires pour chaque nom dans movie_names
-    similar_movies = get_movies_by_name(movie_names, movies_dict)
-    for movie in similar_movies:
-        print(movie)
-        print("\n")
-   # Récupérer les genres de ces films
-    genres = [movie.tags for movie in similar_movies]
-
-    # Compter le nombre d'occurrences de chaque genre
-    genre_counts = Counter(
-        genre for movie_genres in genres for genre in movie_genres)
-
-    # Trouver les genres qui sont présents dans au moins deux des trois films
-    common_genres = [genre for genre,
-                     count in genre_counts.items() if count >= 2]
-
-    same_genre_movies = [movie for movie in movies_dict.values() if len(
-        set(movie.tags) & set(common_genres)) >= 3 and movie.rating != 5.0]
-
-    # Trier ces films par note moyenne et prendre les cinq premiers
-    top_movies = sorted(same_genre_movies,
-                        key=lambda movie: movie.rating, reverse=True)[:limit]
-
-    return top_movies
+    similar_movies = process.extractBests(
+        ' '.join(movie_names), movies_dict.keys(), limit=limit)
+    return [movies_dict[movie_id] for movie_id, _ in similar_movies]
 
 
 @app.route('/')
 def home():
-
-    # Initialiser la liste des films
     movies_dict = MovieInit()
-
-    # Initialiser les noms des films pour lesquels vous voulez trouver des recommandations
     movie_names = ["Toy Story", "Nemo", "Hercule"]
-
-    # Appeler la fonction recommend_movies pour obtenir les recommandations
     recommended_movies = recommend_movies(movie_names, movies_dict)
-
-    # Renvoyer le HTML généré à partir du modèle et des films recommandés
-    return render_template('index.html', movies=recommended_movies)
+    return render_template('code/index.html', movies=recommended_movies)
 
 
 if __name__ == '__main__':
-    # calculate_average_ratings()
-
     average_ratings = {}
-    # Charger les notes moyennes au démarrage du programme
     load_average_ratings()
     app.run(debug=True)
