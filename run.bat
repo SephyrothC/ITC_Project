4a9642810c0ab8add94d974bde8324eb10e8615b
SephyrothC <as.grifon@gmail.com>
2023-12-10 15:19:00 +0900

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,20 +1,26 @@
 @echo off
+setlocal enabledelayedexpansion
 set datetime=%date:~-4,4%_%date:~-7,2%_%date:~-10,2%_%time:~0,2%h%time:~3,2%
 echo Checking and installing necessary Python libraries...
 
 python -m pip install --upgrade pip
 
-python -c "import csv" 2> NUL
-if errorlevel 1 pip install csv
-
-python -c "import collections" 2> NUL
-if errorlevel 1 pip install collections-Counter
-
-python -c "import fuzzywuzzy" 2> NUL
-if errorlevel 1 pip install fuzzywuzzy
-
-python -c "import flask" 2> NUL
-if errorlevel 1 pip install Flask
+set "libraries=csv collections fuzzywuzzy flask"
+set /p "all=Do you want to install all libraries at once? (Y/N) "
+if /i "%all%"=="Y" (
+    for %%a in (%libraries%) do (
+        python -c "import %%a" 2> NUL
+        if errorlevel 1 pip install %%a
+    )
+) else (
+    for %%a in (%libraries%) do (
+        python -c "import %%a" 2> NUL
+        if errorlevel 1 (
+            set /p "install=Do you want to install %%a? (Y/N) "
+            if /i "!install!"=="y" pip install %%a
+        )
+    )
+)
 
 echo Libraries checked and installed successfully.
 
